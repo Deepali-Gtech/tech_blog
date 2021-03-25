9de45135c32839f36bf39d76e02f18d0eedaf3e7
Deepali Garg <deepaligarg.chd@gmail.com>
2021-03-24 21:42:38 -0400

    Added handlebars view

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js
+const hbs = exphbs.create();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log('Now listening on http://localhost:' + PORT)
+  );
 });
