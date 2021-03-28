fda518351b8abca0abddc785b5596b79bf83cded
Deepali Garg <deepaligarg.chd@gmail.com>
2021-03-27 22:22:14 -0400

    code cleanup

--- public/js/blog.js
+++ public/js/blog.js
@@ -22,6 +22,26 @@ const newFormHandler = async (event) => {
     }
   };
 
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blogs/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.delete-blog')
+    .addEventListener('click', delButtonHandler);
+
   document
   .querySelector('.new-project-form')
   .addEventListener('submit', newFormHandler);
