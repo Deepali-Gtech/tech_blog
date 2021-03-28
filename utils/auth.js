eed6809fe1ef1ff5235764f89e366fe86e8b09e1
Deepali Garg <deepaligarg.chd@gmail.com>
2021-03-27 21:13:50 -0400

    Delete blog

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,4 @@
 const withAuth = (req, res, next) => {
-  console.log("====with auth");
-  console.log(req.path);
-  console.log(req.session.logged_in);
   // If the user is not logged in, redirect the request to the login route
   if (!req.session.logged_in) {
     res.redirect('/login');
